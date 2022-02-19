086caaa986ef198fc6f50e59508368d4edf02fcc
Sara Vieira <hey@iamsaravieira.com>
2022-02-19 21:51:02 +0100

    fix some valiudators

--- src/validators.js
+++ src/validators.js
@@ -15,61 +15,59 @@ const removeNativeKeys = (req, res, next) => {
 const sizeValidator = (req, res, next) => {
   if (req.method === "POST" || req.method === "PUT") {
     if (Object.keys(req.body).length > 0) {
-      const memorySize = helper.memorySizeOf(req.body);
-      req["bodySize"] = memorySize;
-
-      // memorySize is size in bytes. 10KB  => 10 * 1024
-      if (memorySize > 10 * 1024) {
-        throwError("JSON body is too large. Should be less than 10KB", 413);
-      } else if (Array.isArray(req.body)) {
-        if (req.body.length > 1000) {
-          throwError("Not more than 1000 records for bulk upload.", 413);
-        } else next();
-      } else next();
+      next();
     } else throwError("Empty body.", 400);
   } else next();
 };
 
 // The Body top level keys should start with an alphabet
 const keysValidator = (req, res, next) => {
-	let validKeys = Array.isArray(req.body) ? req.body.every(helper.isValidKeys) : helper.isValidKeys(req.body);
-	if (validKeys) next();
-	else throwError("Invalid JSON keys. Keys should start with an alphabet");
+  let validKeys = Array.isArray(req.body)
+    ? req.body.every(helper.isValidKeys)
+    : helper.isValidKeys(req.body);
+  if (validKeys) next();
+  else throwError("Invalid JSON keys. Keys should start with an alphabet");
 };
 
 // extract the box, collection, record ids from the path
 const extractParams = (req, res, next) => {
-	const path = req.path;
-	const pathParams = path.split("/").filter((p) => !!p);
-	const isHexString = /^([0-9A-Fa-f]){24}$/;
-	const isValidBoxID = /^[0-9A-Za-z_]+$/i;
+  const path = req.path;
+  const pathParams = path.split("/").filter((p) => !!p);
+  const isHexString = /^([0-9A-Fa-f]){24}$/;
+  const isValidBoxID = /^[0-9A-Za-z_]+$/i;
 
-	req["apiKey"] =
-		req.headers["x-api-key"] || (req.headers["authorization"] ? req.headers["authorization"].split(" ")[1] : null);
+  req["apiKey"] =
+    req.headers["x-api-key"] ||
+    (req.headers["authorization"]
+      ? req.headers["authorization"].split(" ")[1]
+      : null);
 
-	if (pathParams[0]) {
-		req["box"] = isValidBoxID.test(pathParams[0]) ? pathParams[0] : undefined;
+  if (pathParams[0]) {
+    req["box"] = isValidBoxID.test(pathParams[0]) ? pathParams[0] : undefined;
 
-		if (pathParams[1]) {
-			const isObjectId = isHexString.test(pathParams[1]);
-			if (isObjectId) req["recordId"] = pathParams[1];
-			else req["collection"] = isValidBoxID.test(pathParams[1]) ? pathParams[1] : undefined;
-		}
+    if (pathParams[1]) {
+      const isObjectId = isHexString.test(pathParams[1]);
+      if (isObjectId) req["recordId"] = pathParams[1];
+      else
+        req["collection"] = isValidBoxID.test(pathParams[1])
+          ? pathParams[1]
+          : undefined;
+    }
 
-		if (!req["recordId"] && pathParams[2]) {
-			req["recordId"] = isHexString.test(pathParams[2]) ? pathParams[2] : undefined;
-		}
+    if (!req["recordId"] && pathParams[2]) {
+      req["recordId"] = isHexString.test(pathParams[2])
+        ? pathParams[2]
+        : undefined;
+    }
 
-		next();
-	} else throwError("Box id cannot be empty.");
+    next();
+  } else throwError("Box id cannot be empty.");
 };
 
 // check if all the required parameters is present
 const validateParams = (req, res, next) => {
-  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-
   if (!req.box) {
-    throwError("Invalid or empty box id");
+    throwError("Empty box id");
   } else if (req.method === "PUT" || req.method === "DELETE") {
     if (!req.recordId && !req.query.q) {
       throwError("Invalid or empty record id or missing query definition");
